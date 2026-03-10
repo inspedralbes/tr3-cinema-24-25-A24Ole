98cece5cfc8028d1988ec3dfaf44f59ce25cc17e
A24Ole <a24oleproyat@inspedralbes.cat>
2026-03-10 10:08:49 +0100

    3.1.8 Actualización correo de confirmación

--- backend/resources/views/emails/reserva/confirmada.blade.php
+++ backend/resources/views/emails/reserva/confirmada.blade.php
@@ -1,22 +1,32 @@
 <x-mail::message>
-# ¡Reserva Confirmada en CinemaFlow! 🍿
+# 🎬 ¡Tu entrada para la Gran Pantalla! 🍿
 
 Hola **{{ $reserva->nombre }}**,
 
-¡Tus entradas están aseguradas! Prepárate para una experiencia inmersiva. A continuación tienes los detalles de tu compra:
+¡Tu reserva en **CinemaFlow** se ha completado con éxito! Prepárate para disfrutar de la mejor experiencia cinematográfica.
+
+<div style="text-align: center; margin-bottom: 20px;">
+@if($reserva->pelicula && $reserva->pelicula->poster_url)
+<img src="{{ $reserva->pelicula->poster_url }}" alt="{{ $reserva->pelicula->titulo }}" style="width: 200px; border-radius: 10px; box-shadow: 0 4px 15px rgba(0,0,0,0.5);">
+@endif
+</div>
 
 <x-mail::panel>
-### Detalles de la Reserva
-- **Película:** {{ $reserva->pelicula->titulo ?? 'N/A' }}
-- **Fecha y Hora:** {{ $reserva->fecha_reserva }}
-- **Asientos:** {{ collect($reserva->asientos)->pluck('asiento_id')->implode(', ') }}
-- **Total Pagado:** ${{ number_format($reserva->total_pagado, 2) }}
+## 🎫 TICKET DIGITAL
+---
+**Película:** {{ $reserva->pelicula->titulo ?? 'N/A' }}  
+**Fecha:** {{ \Carbon\Carbon::parse($reserva->created_at)->format('d/m/Y H:i') }}  
+**Butacas:** {{ collect($reserva->asientos)->pluck('asiento_id')->implode(', ') }}  
+**Total:** {{ number_format($reserva->total_pagado, 2) }}€
+---
 </x-mail::panel>
 
-<x-mail::button :url="'http://localhost:3000/booking/' . $reserva->id . '-confirmation'">
+<x-mail::button :url="config('app.url') . '/sesion/' . $reserva->pelicula_id . '/asientos'">
 Ver Eticket Online
 </x-mail::button>
 
-Gracias,<br>
-El equipo de {{ config('app.name') }}
+*Si tienes cualquier duda, puedes responder a este correo.*
+
+Gracias por elegirnos,<br>
+**El equipo de {{ config('app.name') }}**
 </x-mail::message>
